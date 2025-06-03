e11c455b9d99c7b1f4e8c2623a0de7f5cd0c2b2b
Bhawani Singh <bhawanisingh0038@gmail.com>
2025-06-03 19:04:31 +0530

    initial commit

---
+++ database.py
@@ -0,0 +1,18 @@
+class DbManagement:
+    def __init__(self):
+        pass
+    
+    def create_table_into_database(self):
+        pass
+    
+    def connect_to_database(self):
+        pass
+    
+    def check_balance(self):
+        pass
+    
+    def withdraw_balance(self):
+        pass
+    
+    def transfer_money(self):
+        pass    
